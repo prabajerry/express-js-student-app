cb0e5618dd9c886353438251b0949e1b1ab7a107
prabajerry <padmakalai2017@gmail.com>
2022-08-21 22:09:46 +0530

    check in sanitize data

--- middlewars/sanitization/index.js
+++ middlewars/sanitization/index.js
@@ -23,46 +23,49 @@ const sanitization_xssStdAge = (req, res, next) => {
 
 }
 const sanitization_xsslanguageKnownInTamil = (req, res, next) => {
-     
+
     if (req.body.userLangu) {
-        
+
         let element = req.body.userLangu
         console.log(element);
-        
+
         let cleanElement = xss(element)
         console.log(cleanElement);
         if (element == cleanElement) {
             console.log("test passed");
             next()
-            
+
         } else {
             throw new Error("xss attack")
-            
+
         }
     }
-    else{
+    else {
         throw new Error("empty data")
     }
 }
 const sanitization_xssfindElibleStd = (req, res, next) => {
+    
     let sanitizeData = [req.body.userGender, req.body.userAge, req.body.userCountry, req.body.userLanguage]
-    try {
-        const checkLength = helper.findArrayLength(sanitizeData)
-        
-        if (checkLength) {
-            for (let index = 0; index < sanitizeData.length; index++) {
-                const element = sanitizeData[index];
-                let cleanedData = xss(element)
-                if (element == cleanedData) {
-                    next()
-                } else {
-                    throw new Error("xss attack")
+        try {
+            const checkLength = helper.findArrayLength(sanitizeData)
+
+            if (checkLength) {
+                for (let index = 0; index < sanitizeData.length; index++) {
+                    const element = sanitizeData[index];
+                    let cleanedData = xss(element)
+                    if (element == cleanedData) {
+                        next()
+                    } else {
+                        throw new Error("xss attack")
+                    }
                 }
             }
+            
+        } catch (error) {
+            throw error
         }
-    } catch (error) {
-        throw error
-    }
+        
 
 
 }
@@ -83,16 +86,22 @@ const sanitization_xssfindDetail = (req, res, next) => {  // task
 
 }
 const sanitization_xssfindLang = (req, res, next) => {
-    let userLanguData = req.body.userLangu
-    console.log(userLanguData);
-    let cleanUesrLanguData = xss(userLanguData)
-    if (userLanguData == cleanUesrLanguData) {
-        console.log("testcase passed");
-        next()
-
-    } else {
-        throw new Error("xss attack")
-
+    if (req.body.userLangu) {
+        
+        let userLanguData = req.body.userLangu
+        console.log(userLanguData);
+        let cleanUesrLanguData = xss(userLanguData)
+        if (userLanguData == cleanUesrLanguData) {
+            console.log("testcase passed");
+            next()
+            
+        } else {
+            throw new Error("xss attack")
+            
+        }
+    }
+    else{
+        throw new Error("empty data")
     }
 
 }
